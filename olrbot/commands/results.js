c89a52aef453d4017c9ea4ea31cf8f440effa377
Kyle Crouse <kycrouse@amazon.com>
2020-12-06 14:58:07 +0000

    adding lib for scraping danlisa and updating results command

--- olrbot/commands/results.js
+++ olrbot/commands/results.js
@@ -1,4 +1,6 @@
-const importer = require(`${process.cwd()}/lib/scraper/results`);
+const Discord = require('discord.js');
+const getResults = require(`${process.cwd()}/lib/scraper/results`);
+const { tracks } = require ('../../constants');
 const REACTION_SUCCESS = '✅';
 const REACTION_FAILURE = '😢';
 
@@ -9,14 +11,19 @@ module.exports = {
   usage: '<race_id from danlisa.com>',
 	execute: async (message, args) => {
 
-    try {
-      await importer(args[0]);
-      message.react(REACTION_SUCCESS);
-    }
-    catch(error) {
-      console.log(error);
-      message.react(REACTION_FAILURE);
-    }
+    const race = await getResults(args[0]);
+
+    const embed = new Discord.MessageEmbed()
+    	.setTitle(race.name)
+    	.setURL(`http://dnhi063vpnzuy.cloudfront.net/race/${args[0]}/`)
+    	.setThumbnail(tracks.find(({ name }) => race.track.indexOf(name) >= 0).logo)
+    	.addFields(
+    		{ name: 'Inline field title', value: 'Some value here', inline: true },
+    		{ name: 'Inline field title', value: 'Some value here', inline: true },
+    	)
+    	.setTimestamp()
+      
+    message.channel.send(embed);
 
 	},
 };
