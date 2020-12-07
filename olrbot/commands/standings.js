7d3d93603fef636d0426e1911d034c6b349d94b5
Kyle Crouse <kycrouse@amazon.com>
2020-12-07 23:00:37 +0000

    converting standings to output

--- olrbot/commands/standings.js
+++ olrbot/commands/standings.js
@@ -1,20 +1,31 @@
-const getStandings = require(`${process.cwd()}/lib/scraper/standings`);
-const REACTION_SUCCESS = '✅';
-const REACTION_FAILURE = '😢';
+const Discord = require('discord.js');
+const league = require(`${process.cwd()}/lib/league`);
 
 module.exports = {
 	name: 'standings',
-	description: 'Import standings for a specific season from danlisa.com',
-  args: true,
-  usage: '<season id>',
-	execute: async (message, args) => {
-    try {
-      const standings = await getStandings(args[0]);
-      message.react(REACTION_SUCCESS);
-    }
-    catch(error) {
-      console.log(error);
-      message.react(REACTION_FAILURE);
-    }
+	description: 'Display standings for the current season',
+  args: false,
+	execute: async (message) => {
+    
+    await league.init();
+    
+    const { fields: season } = league.season;
+    
+    const standings = season.standings['en-US'].sort((a,b) => a.position - b.position);
+
+    const embed = new Discord.MessageEmbed()
+    	.setTitle('Current Standings')
+    	// .setURL(`http://dnhi063vpnzuy.cloudfront.net/standings/`)
+      .setDescription(`After ${season.results['en-US'].length} of ${season.schedule['en-US'].length} races`)
+      .setThumbnail('http://output-racing.s3-website.us-west-2.amazonaws.com/logo.png')
+    	.addFields(
+    		{ name: 'Pos.', value: standings.map(item => item.position).join('\u000a'), inline: true },
+    		{ name: 'Driver', value: standings.map(item=> item.driver).join('\u000a'), inline: true },
+    		{ name: 'Behind', value: standings.map(item=> item.behindNext).join('\u000a'), inline: true },
+    	)
+    	.setTimestamp()
+      
+    message.channel.send(embed);
+
 	},
 };
