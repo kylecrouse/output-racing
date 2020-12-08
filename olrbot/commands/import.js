143e99733060cfcf01035f622739bad2d9eb1ef8
Kyle Crouse <kycrouse@amazon.com>
2020-12-08 19:24:46 +0000

    adding logo and media to race results embed

--- olrbot/commands/import.js
+++ olrbot/commands/import.js
@@ -5,6 +5,7 @@ const getSeason = require(`${process.cwd()}/lib/scraper/season`);
 const getStandings = require(`${process.cwd()}/lib/scraper/standings`);
 const getStats = require(`${process.cwd()}/lib/scraper/stats`);
 const league = require(`${process.cwd()}/lib/league`);
+const cms = require(`${process.cwd()}/lib/contentful`);
 const { isAuthorized } = require('../lib/authorization');
 const { tracks } = require ('../../constants');
 const REACTION_SUCCESS = '✅';
@@ -85,7 +86,6 @@ async function handleResults(message, args) {
   const embed = new Discord.MessageEmbed()
   	.setTitle(race.fields.name['en-US'])
   	.setURL(`http://dnhi063vpnzuy.cloudfront.net/race/${args[1]}/`)
-  	.setThumbnail(tracks.find(({ name }) => race.fields.track['en-US'].indexOf(name) >= 0).logo)
   	.addFields(
   		{ name: 'P', value: results.map(item => item.finish).join('\u000a'), inline: true },
   		{ name: 'Driver', value: results.map(item=> item.name).join('\u000a'), inline: true },
@@ -93,6 +93,21 @@ async function handleResults(message, args) {
   	)
   	.setTimestamp()
     
+  if (race.fields.logo) {
+    const logo = await cms.getAsset(race.fields.logo['en-US'].sys.id);
+    embed.setThumbnail(`https:${logo.fields.file['en-US'].url}`);
+  } else {
+    embed.setThumbnail(
+      tracks.find(({ name }) => race.fields.track['en-US'].indexOf(name) >= 0).logo
+    );
+  }
+  
+  if (race.fields.media) {
+    const asset = race.fields.media['en-US'].shift();
+    const media = await cms.getAsset(asset.sys.id);
+    embed.setImage(`https:${media.fields.file['en-US'].url}`);
+  }
+
   message.react(REACTION_SUCCESS);
   message.channel.send(embed);  
 }
