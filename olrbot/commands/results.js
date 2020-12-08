4f51d918f54cdcee5dae47f1c3a8f29855d73cac
Kyle Crouse <kycrouse@amazon.com>
2020-12-08 19:16:50 +0000

    converting results command to be read-only and fixing bug with getting past results by track

--- olrbot/commands/results.js
+++ olrbot/commands/results.js
@@ -1,46 +1,38 @@
 const Discord = require('discord.js');
-const getResults = require(`${process.cwd()}/lib/scraper/results`);
+const league = require(`${process.cwd()}/lib/league`);
 const { tracks } = require ('../../constants');
 const REACTION_SUCCESS = '✅';
 const REACTION_FAILURE = '😢';
 
 module.exports = {
 	name: 'results',
-	description: 'Import a specific race result from danlisa.com, optionally providing event name, broadcast URL from YouTube and attaching screenshot image',
-  args: true,
-  usage: '<race_id from danlisa.com> "<event name>" <broadcast url>',
+	description: 'Display results for latest race, or last at a specified track',
+  args: false,
+  usage: '[<track name>]',
 	execute: async (message, args) => {
+    
+    await league.init();
 
-    try {
-      let overrides = {};
-      if (args.length > 1) overrides.name = args[1];
-      if (args.length > 2) overrides.broadcast = args[2];
-      // TODO: Allow attachment to be sent through as a "media" override,
-      //       though this requires merging arrays on the cms.update end
+    const race = await league.getLastRace(args.length > 0 ? { track: args[0] } : {});
+    
+    if (!race) return;
+    
+    const results = race.fields.results['en-US'].sort((a,b) => a.finish - b.finish);
+
+    const embed = new Discord.MessageEmbed()
+    	.setTitle(race.fields.name['en-US'])
+    	.setURL(`http://dnhi063vpnzuy.cloudfront.net/race/${args[0]}/`)
+    	.setThumbnail(tracks.find(({ name }) => race.fields.track['en-US'].indexOf(name) >= 0).logo)
+    	.addFields(
+    		{ name: 'P', value: results.map(item => item.finish).join('\u000a'), inline: true },
+    		{ name: 'Driver', value: results.map(item=> item.name).join('\u000a'), inline: true },
+    		{ name: 'Interval', value: results.map(item=> item.interval).join('\u000a'), inline: true },
+    	)
+    	.setTimestamp()
       
-      const race = await getResults(args[0], overrides);
+    message.react(REACTION_SUCCESS);
       
-      const results = race.fields.results['en-US'].sort((a,b) => a.finish - b.finish).slice(0,5);
-  
-      const embed = new Discord.MessageEmbed()
-      	.setTitle(race.fields.name['en-US'])
-      	.setURL(`http://dnhi063vpnzuy.cloudfront.net/race/${args[0]}/`)
-      	.setThumbnail(tracks.find(({ name }) => race.fields.track['en-US'].indexOf(name) >= 0).logo)
-      	.addFields(
-      		{ name: 'P', value: results.map(item => item.finish).join('\u000a'), inline: true },
-      		{ name: 'Driver', value: results.map(item=> item.name).join('\u000a'), inline: true },
-      		{ name: 'Interval', value: results.map(item=> item.interval).join('\u000a'), inline: true },
-      	)
-      	.setTimestamp()
-        
-      message.react(REACTION_SUCCESS);
-        
-      message.channel.send(embed);      
-    }
-    catch(error) {
-      console.log(error);
-      message.react(REACTION_FAILURE);
-    }
+    message.channel.send(embed);      
 
 	},
 };
