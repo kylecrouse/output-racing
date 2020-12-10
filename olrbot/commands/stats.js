ffa487df0ad702edb120dfa8e4d5df48640effd3
Kyle Crouse <kycrouse@amazon.com>
2020-12-10 10:39:56 -0800

    changing stats to default to author and optionally allow multiple mentions

--- olrbot/commands/stats.js
+++ olrbot/commands/stats.js
@@ -3,76 +3,84 @@ const fetch = require('node-fetch');
 const sharp = require('sharp');
 const league = require(`${process.cwd()}/lib/league`);
 const cms = require(`${process.cwd()}/lib/contentful`)
-const util = require('util');
-const REACTION_SUCCESS = '✅';
-const REACTION_FAILURE = '😢';
 
 module.exports = {
 	name: 'stats',
-	description: 'Show career stat line for a specific driver',
-  args: true,
-  usage: '@<user>',
+	description: 'Show career stat line for yourself, or for specific driver(s)',
+  args: false,
+  usage: '[@<user>]',
 	execute: async (message, args) => {
-    
-    // Ensure data is ready
-    await league.init();
-    
-    // Get out of here if no one was mentioned
-    if (!message.mentions.users) return message.react(REACTION_FAILURE);
-    
-    // Find all the mentioned drivers and translate to embed
-    await Promise.all(
-      message.mentions.users.map(async (user) => {
-        
-        const { fields: driver } = league.findDriver({ field: "discordId", value: user.id });
-        
-        if (!driver) {
-          message.reply("I don't know which driver profile is yours. Use `!link <your car number>` and then try again.");
-          return;
-        }
-        
-        const stats = league.getStats(driver.name['en-US']);
-        
-        const embed = new Discord.MessageEmbed()
-        	.setAuthor(
-            message.guild && message.guild.member(user) 
-              ? message.guild.member(user).displayName 
-              : user.username, 
-            user.displayAvatarURL()
-          )
-        	.setTitle('Output Racing League Career Stats')
-        	.setURL(`http://dnhi063vpnzuy.cloudfront.net/driver/${driver.name['en-US'].replace(/\s/g, '-').toLowerCase()}/`)
-        	.addFields(
-        		{ name: 'Starts', value: stats.starts, inline: true },
-        		{ name: 'Wins', value: `${stats.wins} (${stats.winPercentage})`, inline: true },
-        		{ name: 'Top 5s', value: `${stats.top5s} (${stats.top5Percentage})`, inline: true },
-        		{ name: 'Poles', value: `${stats.poles} (${((stats.poles || 0) / (stats.starts || 1) * 100).toFixed(0)}%)`, inline: true },
-        		{ name: 'Laps Led', value: `${stats.lapsLed} (${((parseInt(stats.lapsLed.replace(',','')) / parseInt((stats.laps || '0').replace(',',''))) * 100).toFixed(0)}%)`, inline: true },
-        		{ name: 'Inc/Lap', value: `${stats.incidentsLap}`, inline: true },
-        	)
-        	.setTimestamp();
-          
-        if (driver.numberArt) {
-          const numberArt = await cms.getAsset(driver.numberArt['en-US'].sys.id);
-          if (numberArt.fields.file['en-US'].contentType === 'image/svg+xml') {
-            const response = await fetch(`https:${numberArt.fields.file['en-US'].url}`);
-            const buffer = await response.buffer();
-            embed.attachFiles([{ name: 'number.png', attachment: await sharp(buffer).toBuffer() }]);
-            embed.setThumbnail('attachment://number.png');
-          } 
-          else 
-            embed.setThumbnail(`https:${numberArt.fields.file['en-US'].url}`);
-        }
-        
-        if (driver.media) {
-          const asset = driver.media['en-US'].pop();
-          const media = await cms.getAsset(asset.sys.id);
-          embed.setImage(`https:${media.fields.file['en-US'].url}`);
-        }
-          
-        message.channel.send(embed);
-      })
-    );
+
+    // Find all the mentioned users and translate to embed
+    if (message.mentions.users.size > 0)
+      await Promise.all(
+        message.mentions.users.map(user => sendStats(user, message))
+      );
+
+    // Otherwise show stats for the author
+    else 
+      await sendStats(message.author, message);      
 
 	},
 };
+
+async function sendStats(user, message) {
+  const embed = await getStats(
+    // Use guild member if present, otherwise just user
+    message.guild && message.guild.member(user) 
+      ? message.guild.member(user)
+      : user
+  );
+
+  if (embed) message.channel.send(embed);
+}
+
+async function getStats(user) {
+  // Ensure data is ready
+  await league.init();
+  
+  const { fields: driver } = league.findDriver(
+    { field: "discordId", value: user.id }
+  );
+    
+  if (!driver) return undefined;
+  
+  const stats = league.getStats(driver.name['en-US']);
+  
+  const embed = new Discord.MessageEmbed()
+  	.setAuthor(
+      user.displayName || user.username,
+      user.displayAvatarURL()
+    )
+  	.setTitle('Output Racing League Career Stats')
+  	.setURL(`http://dnhi063vpnzuy.cloudfront.net/driver/${driver.name['en-US'].replace(/\s/g, '-').toLowerCase()}/`)
+  	.addFields(
+  		{ name: 'Starts', value: stats.starts, inline: true },
+  		{ name: 'Wins', value: `${stats.wins} (${stats.winPercentage})`, inline: true },
+  		{ name: 'Top 5s', value: `${stats.top5s} (${stats.top5Percentage})`, inline: true },
+  		{ name: 'Poles', value: `${stats.poles} (${((stats.poles || 0) / (stats.starts || 1) * 100).toFixed(0)}%)`, inline: true },
+  		{ name: 'Laps Led', value: `${stats.lapsLed} (${((parseInt(stats.lapsLed.replace(',','')) / parseInt((stats.laps || '0').replace(',',''))) * 100).toFixed(0)}%)`, inline: true },
+  		{ name: 'Inc/Lap', value: `${stats.incidentsLap}`, inline: true },
+  	)
+  	.setTimestamp();
+    
+  if (driver.numberArt) {
+    const numberArt = await cms.getAsset(driver.numberArt['en-US'].sys.id);
+    if (numberArt.fields.file['en-US'].contentType === 'image/svg+xml') {
+      const response = await fetch(`https:${numberArt.fields.file['en-US'].url}`);
+      const buffer = await response.buffer();
+      embed.attachFiles([{ name: 'number.png', attachment: await sharp(buffer).toBuffer() }]);
+      embed.setThumbnail('attachment://number.png');
+    } 
+    else 
+      embed.setThumbnail(`https:${numberArt.fields.file['en-US'].url}`);
+  }
+  
+  if (driver.media) {
+    const asset = driver.media['en-US'].pop();
+    const media = await cms.getAsset(asset.sys.id);
+    embed.setImage(`https:${media.fields.file['en-US'].url}`);
+  }
+  
+  return embed;
+}
