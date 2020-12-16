78ba3a9ff2c9fc3b0fb97c9fc80ceb880a0accb9
Kyle Crouse <kycrouse@amazon.com>
2020-12-16 18:43:11 +0000

    fixing standings to only count points races

--- olrbot/commands/standings.js
+++ olrbot/commands/standings.js
@@ -14,7 +14,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     	.setTitle('Current Standings')
     	.setURL(`http://dnhi063vpnzuy.cloudfront.net/standings/${league.season.id}/`)
-      .addField(league.season.name, `After ${league.season.results.length} of ${league.season.schedule.length} races`)
+      .addField(league.season.name, `After ${league.season.results.length} of ${league.season.schedule.filter(race => race.counts).length} races`)
       .setThumbnail('http://output-racing.s3-website.us-west-2.amazonaws.com/logo-stacked.png')
     	.addFields(
     		{ name: 'Pos.', value: standings.map(item => `\`${item.position} ${item.change !== '-' ? '(' + item.change + ')' : ''}\``), inline: true },
