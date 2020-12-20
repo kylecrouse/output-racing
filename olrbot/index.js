d09261bd365b0ec6b79abd7f5063811ea1fac591
Kyle Crouse <kycrouse@amazon.com>
2020-12-19 16:28:11 -0800

    moving messages handler back for the time being

--- olrbot/index.js
+++ olrbot/index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const handleGuildMemberAdd = require('./handlers/guildMemberAdd');
 const handleMessage = require('./handlers/message');
 const { handleApplication } = require('./lib/applications');
+const { prefix, superUsers } = require('./config.json');
 
 const client = new discord.Client();
 
@@ -21,7 +22,52 @@ for (const file of commandFiles) {
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-client.on('message', handleMessage);
+client.on('message', async (message) => {
+  // console.log(message);
+  
+  // Exit if there is no command or message is from a bot.
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+  // Only handle guild messages or DMs from allowed users
+  if (!(message.guild || superUsers.includes(message.author.id))) return;
+    
+  // Get message command (i.e., !command) and args
+  // Use regex that doesn't split inside quotes but doesn't match them either
+  const regex = /[^\s"]+|"([^"]*)"/gi;
+  const string = message.content.slice(prefix.length).trim();
+  let args = [], match = null;
+  //Each call to exec returns the next regex match as an array
+  do {
+    match = regex.exec(string);
+    if (match) args.push(match[1] ? match[1] : match[0]);
+  } while (match);
+  const commandName = args.shift().toLowerCase();
+
+  // Get the requested command
+  const command = client.commands.get(commandName)
+    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));;
+  
+  // If the command is not recognized, exit.  
+  if (!command) return;
+  
+  // Validate required args or exit with proper usage
+  if (command.args && !args.length) {
+    let reply = `You're missing something, ${message.author}.`;
+    if (command.usage) {
+      reply += `\nTry: \`${prefix}${command.name} ${command.usage}\``;
+    }
+    return message.channel.send(reply);
+  }
+
+  // Execute requested command
+  try {
+  	command.execute(message, args);
+  } catch (error) {
+  	console.error(error);
+    message.reply('```' + JSON.stringify(error) + '```');
+  }
+  
+});
 client.on('guildMemberAdd', handleGuildMemberAdd);
 
 client.login(process.env.DISCORD_ACCESS_TOKEN);
