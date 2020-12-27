f9e57c2c841d7d5c25e1696792580e982a72755f
Kyle Crouse <kycrouse@amazon.com>
2020-12-27 10:35:54 -0800

    splitting build commands out in to multiple steps for logging purposes

--- olrbot/commands/build.js
+++ olrbot/commands/build.js
@@ -12,7 +12,8 @@ module.exports = {
     if (!isAuthorized(message.author, message.channel)) return;
       
     console.log('Building and deploying website...');
-    const build = spawn('npm run build && aws s3 sync ./out s3://output-racing/ && aws cloudfront create-invalidation --distribution-id E2HCYIFSR21K3R', { shell: true });
+
+    const build = spawn('npm', ['run', 'build']);
     
     build.stdout.on('data', (data) => {
       console.log(`Build stdout: ${data}`);
@@ -20,7 +21,53 @@ module.exports = {
     
     build.on('exit', (code) => {
       console.log(`Build exited with code: ${code}`);
-      message.react(REACTION_SUCCESS);
+      
+      const sync = spawn(
+        'aws', 
+        ['s3', 'sync', './out', 's3://output-racing/']
+      );
+      
+      sync.stdout.on('data', (data) => {
+        console.log(`Sync stdout: ${data}`);
+      });
+      
+      sync.on('exit', (code) => {
+        console.log(`Sync exited with code: ${code}`);
+        
+        const invalidate = spawn(
+          'aws', 
+          ['cloudfront', 'create-invalidation', '--distribution-id', 'E2HCYIFSR21K3R']
+        );
+
+        invalidate.stdout.on('data', (data) => {
+          console.log(`Invalidate stdout: ${data}`);
+        });
+        
+        sync.on('exit', (code) => {
+          console.log(`Invalidate exited with code: ${code}`);
+          message.react(REACTION_SUCCESS);
+        });
+        
+        invalidate.on('error', (err) => {
+          console.error(err);
+          message.react(REACTION_FAILURE);
+        });
+    
+        invalidate.on('close', (code) => {
+          console.log(`Invalidate closed with code: ${code}`);
+        });
+
+      });
+
+      sync.on('error', (err) => {
+        console.error(err);
+        message.react(REACTION_FAILURE);
+      });
+  
+      sync.on('close', (code) => {
+        console.log(`Sync closed with code: ${code}`);
+      });
+      
     });
     
     build.on('error', (err) => {
