09b5690282f644d1262bd81feb3ef882cdb5cf7f
Kyle Crouse <kycrouse@amazon.com>
2021-01-13 21:31:17 -0800

    added arca car

--- constants.js
+++ constants.js
@@ -1,6 +1,10 @@
 module.exports = {
   leagueId: 1710,
   cars: [
+    { id: 24,
+      name: 'ARCA Menards Chevrolet Impala',
+      image: 'https://ir-core-sites.iracing.com/members/member_images/cars/impalanational/profile.jpg'
+    },
     { id: 45,
       name: '[Legacy] NASCAR Cup Chevrolet SS - 2013',
       image: 'https://d3bxz2vegbjddt.cloudfront.net/members/member_images/cars/carid_45/profile.jpg'
