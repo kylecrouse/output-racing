c00bc52098ccefeccda1bc90fde1209b5129d082
Kyle Crouse <kycrouse@amazon.com>
2021-01-13 21:53:35 -0800

    adding fallback for no stats

--- constants.js
+++ constants.js
@@ -174,6 +174,15 @@ module.exports = {
     },
     { name: 'Rockingham Speedway',
       logo: 'https://d3bxz2vegbjddt.cloudfront.net/members/member_images/tracks/rockingham/logo.jpg'
+    },
+    { name: 'Nashville Fairgrounds Speedway',
+      logo: 'https://ir-core-sites.iracing.com/members/member_images/tracks/nashville/logo.jpg'
+    },
+    { name: 'North Wilkesboro Speedway',
+      logo: 'https://ir-core-sites.iracing.com/members/member_images/tracks/northwilkesboro/logo.jpg'
+    },
+    { name: 'Tsukuba Circuit',
+      logo: 'https://ir-core-sites.iracing.com/members/member_images/tracks/tsukuba/logo.jpg'
     }
   ]
 }
