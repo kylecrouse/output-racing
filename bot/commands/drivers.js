06e8d9496279d1cca16b16fe8dca910b461bf52f
Kyle Crouse <kycrouse@amazon.com>
2021-02-08 13:55:25 -0800

    chunking drivers embed in to subsections to avoid discord size limit

--- bot/commands/drivers.js
+++ bot/commands/drivers.js
@@ -17,12 +17,26 @@ module.exports = {
     	.setTitle('Output Racing League Drivers')
     	.setURL(`https://outputracing.com/drivers/`)
       .setThumbnail('http://output-racing.s3-website.us-west-2.amazonaws.com/logo-stacked.png')
-    	.addFields(
-    		{ name: '#', value: drivers.map(driver => `\`${driver.number || '-'}\``), inline: true },
-    		{ name: 'Driver', value: drivers.map(driver => `\`${driver.nickname || driver.name}\``), inline: true },
-    		{ name: 'License', value: drivers.map(driver => `\`${driver.license.licGroupDisplayName.replace('Class ','')} ${driver.license.srPrime}.${driver.license.srSub} / ${(parseInt(driver.license.iRating)/1000).toFixed(1)}k / ${driver.careerStats ? driver.careerStats.avgIncPerRace.toFixed(2) : '?.??'}x\``), inline: true },
-    	)
     	.setTimestamp()
+      
+    let index, length, array, chunk = 25;
+    for (let index = 0, length = drivers.length; index < length; index += chunk) {
+      array = drivers.slice(index, index + chunk);
+      embed.addFields(
+    		{ name: index === 0 ? '#' : '\u200b', 
+          value: array.map(driver => `\`${driver.number || '-'}\``), 
+          inline: true 
+        },
+    		{ name: index === 0 ? 'Driver' : '\u200b', 
+          value: array.map(driver => `\`${driver.nickname || driver.name}\``), 
+          inline: true 
+        },
+    		{ name: index === 0 ? 'License' : '\u200b', 
+          value: array.map(driver => `\`${driver.license.licGroupDisplayName.replace('Class ','')} ${driver.license.srPrime}.${driver.license.srSub} / ${(parseInt(driver.license.iRating)/1000).toFixed(1)}k / ${driver.careerStats ? driver.careerStats.avgIncPerRace.toFixed(2) : '?.??'}x\``), 
+          inline: true 
+        }
+      );
+    }
       
     message.channel.send(embed);
 
