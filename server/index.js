5985b712e749805d9b0d46b99993f95e1f903fac
Kyle Crouse <kycrouse@amazon.com>
2021-02-15 15:34:36 -0800

    logging telemetry

--- server/index.js
+++ server/index.js
@@ -77,8 +77,6 @@ const { handleApplication } = require('../bot/lib/applications');
     });
   }
   
-  console.log(cache.streamers);
-    
   const options = {
     origin: ['http://localhost:3000', 'https://outputracing.com']
   };
@@ -99,38 +97,47 @@ const { handleApplication } = require('../bot/lib/applications');
   });
   
   app.post('/telemetry', bodyParser.json(), (req, res) => {
-    // console.log(req.body);
-    const data = req.body[req.body.type];
-    if (data.sname === 'TESTING') {
-      // Get next race matching track from cache
-      const race = league.getNextRace({ track: data.trackname });
-      if (race) {
-        const d = data.d['0'];
-        // Get matching driver
-        const driver = league.drivers.find(({ name }) => name === d.name);
-        if (driver) {
-          // Get testing data for matching driver
-          const record = race.testing[driver.id];
-          // New best lap set?
-          if (!record || (record && d.b >= record.best.lap)) {
-            // Put testing data with new record
-            race.put({ testing: { 
-              ...race.testing, 
-              [driver.id]: {
-                ...race.testing[driver.id],
-                best: {
-                  date: data.date,
-                  skies: data.skies,
-                  tracktemp: (data.tracktemp * (9/5) + 32).toFixed(0),
-                  lap: d.b,
+    try {
+      // console.log(req.body);
+      const data = req.body[req.body.type];
+      console.log('111', data.sname === 'TESTING');
+      if (data.sname === 'TESTING') {
+        // Get next race matching track from cache
+        const race = league.getNextRace({ track: data.trackname });
+        console.log('222', data.trackname, race);
+        if (race) {
+          const d = data.d['0'];
+          // Get matching driver
+          const driver = league.drivers.find(({ name }) => name === d.name);
+          console.log('333', data.d['0'], driver);
+          if (driver) {
+            // Get testing data for matching driver
+            const record = race.testing[driver.id];
+            // New best lap set?
+            console.log('444', race.testing, record, d.b);
+            if (!record || (record && d.b >= record.best.lap)) {
+              // Put testing data with new record
+              race.put({ testing: { 
+                ...race.testing, 
+                [driver.id]: {
+                  ...race.testing[driver.id],
+                  best: {
+                    date: data.date,
+                    skies: data.skies,
+                    tracktemp: (data.tracktemp * (9/5) + 32).toFixed(0),
+                    lap: d.b,
+                  }
                 }
-              }
-            }});
+              }});
+            }
           }
         }
       }
+      res.send('OK');
+    } catch(err) {
+      console.log(err);
+      res.send('OK');
     }
-    res.send('OK');
   });
   
   app.post('/tpost_img.php', bodyParser.raw(), (req, res) => {
