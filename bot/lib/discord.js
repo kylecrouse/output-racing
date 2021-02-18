a7e992c0756e1c862035ddfddee0850ed0b557b0
Kyle Crouse <kycrouse@amazon.com>
2021-02-18 14:43:23 -0800

    split discord client to its own module. fixes #7.

---
+++ bot/lib/discord.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const { getSecretValue } = require('../../lib/secrets');
+
+// const client = new discord.Client({ ws: { intents: discord.Intents.FLAGS.GUILD_MEMBERS }});
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+getSecretValue('ORLBot/Discord').then(({ accessToken }) => client.login(accessToken));
+
+module.exports = client;
