7bf5982fd70501bd8fef26a342a258e48c12062b
Kyle Crouse <kycrouse@amazon.com>
2021-03-02 13:27:06 -0800

    updating next script to use common discord client

--- bot/scripts/next.js
+++ bot/scripts/next.js
@@ -1,24 +1,25 @@
-const Discord = require('discord.js');
+const client = require('../lib/discord');
 const league = require('../../lib/league');
 const { announcementChannelId } = require('../config.json');
 const { getUpcomingEmbed } = require('../lib/embeds');
 
-const client = new Discord.Client();
+if (client.uptime > 0) {
+  main();
+} else {
+  client.on('ready', main);
+}
 
-client.on('ready', async () => {
+async function main() {
   console.log(`Logged in as ${client.user.tag} for scripts/next.`);
   
-    await league.init();
+  await league.init();
 
-    const race = league.getNextRace();
-    
-    if (!race || race.offWeek /*|| !moment().isSame(race.date, 'day')*/) 
-      return client.destroy();
-    
+  const race = league.getNextRace();
+  
+  if (!race || race.offWeek /*|| !moment().isSame(race.date, 'day')*/) 
+    process.exit(0);
+  else
     await client.channels.cache.get(announcementChannelId).send("@everyone", getUpcomingEmbed(league));
-    
-    client.destroy();
-
-});
-
-client.login(process.env.DISCORD_ACCESS_TOKEN);
+  
+  process.exit(0);
+}
