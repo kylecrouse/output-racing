6e9bc9ce6d5ca3058dcd5ec513e27dbcbf6dc43c
Crouse, Kyle <kycrouse@a078177e207e.local>
2021-04-19 23:41:23 +0000

    updating raceweek

--- bot/scripts/news.js
+++ bot/scripts/news.js
@@ -25,7 +25,7 @@ async function main() {
   // TODO: Is it possible to calculate the raceweek index from schedule so it can be automated?
   
   const series = [
-    [{ seasonid: 3118, raceweek: 8 }, { seasonid: 3119, raceweek: 8 }],
+    [{ seasonid: 3118, raceweek: 9 }, { seasonid: 3119, raceweek: 9 }],
     [{ seasonid: 3122, raceweek: 3 }]
   ];
   
