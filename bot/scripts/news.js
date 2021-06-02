9fbf58bc4c7c565c73dccd49610c0cda47c63e67
Crouse, Kyle <kycrouse@a078177e207e.local>
2021-06-02 18:49:52 +0000

    bumping raceweek and moving up announcement schedule

--- bot/scripts/news.js
+++ bot/scripts/news.js
@@ -25,7 +25,7 @@ async function main() {
   // TODO: Is it possible to calculate the raceweek index from schedule so it can be automated?
   
   const series = [
-    [{ seasonid: 3118, raceweek: 14 }, { seasonid: 3119, raceweek: 14 }],
+    [{ seasonid: 3118, raceweek: 15 }, { seasonid: 3119, raceweek: 15 }],
     [{ seasonid: 3122, raceweek: 6 }]
   ];
   
